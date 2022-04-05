2b8504fcb6dcc57e917fd53b111862a02cdd8976
Ananta Gurung <anantar@live.com>
2022-04-04 23:59:55 -0400

    Create button functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelectorAll('button');
+const p = document.querySelector('canvas');
+
+button.forEach(button => {
+    button.addEventListener('click', function() {
+        let div = document.createElement('div');
+        div.append(/*function() that creates the div grid goes here*/);
+    })
+})
