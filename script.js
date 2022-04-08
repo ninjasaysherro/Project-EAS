870a3e5cec6321cc6d72cbe3aea126636ed7eb03
Ananta Gurung <anantar@live.com>
2022-04-08 00:56:14 -0400

    Add function to take in one variable for two numbers

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const button = document.querySelectorAll('button');
-const p = document.querySelector('canvas');
+const erase = document.getElementById('clearGrid');
+const canvas = document.getElementById('canvas');
+const rows = document.getElementsByClassName('gridRow');
+const cells = document.getElementsByClassName('cell');
 
 button.forEach(button => {
     button.addEventListener('click', function() {
-        let div = document.createElement('div');
-        div.append(/*function() that creates the div grid goes here*/);
+        const selection = button.dataset.selection;
+        addGrid(selection)
     })
 })
+
+//work on this tomorrow
+canvas.addEventListener("onmouseover", e => {
+    canvas.style.backgroundColor = 'black';l
+})
+
+function addGrid(x) {
+    makeRows(x);
+    makeColumns(x);
+}
+
+function makeRows(rowNum) {
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        canvas.appendChild(row).className = "gridRow";
+    }
+}
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        }
+    }
+}
